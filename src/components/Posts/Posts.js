7861effbad827c2d0318c30b30a829994369db85
stergios gkalios <stergiosgk@gmail.com>
2021-03-08 20:27:14 +0200

    changes

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -39,45 +39,43 @@ class Posts extends Component {
         {/* {this.state.posts.map((post) => console.log(post))} */}
 
         {this.state.posts.map((post) => (
-
-        <div className="post-box mb-5 rounded shadow-lg mt-5">
-          <div className="post-title py-2 text-center font-weight-bold border-bottom">
-            {post.title}
-          </div>
-          <div className="post-header p-3 row">
-            <div className="user-photo mr-3 border rounded-circle"></div>
-            <div className="">
-              <div className="mb-2 text-capitalize font-weight-bold">
-                {post.username}
-              </div>
-              <div className="postDateCreated">{post.dateCreated}</div>
+          <div className="post-box mb-5 rounded shadow-lg mt-5" key={post.id}>
+            <div className="post-title py-2 text-center font-weight-bold border-bottom">
+              {post.title}
             </div>
-          </div>
-          <div className="post-description p-3 border-bottom">
-            {post.description}
-          </div>
-          <div className="post-photo container">
-            <img src={`data:image/jpeg;base64,${post.image}`} />
-          </div>
-          <div className="container post-footer d-flex justify-content-between px-4 py-2">
-            <div className="text-secondary">
-              0 <i className="fas fa-heart"></i>
-            </div>
-            <div className="text-secondary">2 Comments</div>
-          </div>
-          <div className="container border-top d-flex justify-content-between px-5 py-3">
-            <div className="likeCommentBtn">
-              <i className="far fa-thumbs-up"></i> Like
-            </div>
-            <Link to="/comments" className="likeCommentBtn">
+            <div className="post-header p-3 row">
+              <div className="user-photo mr-3 border rounded-circle"></div>
               <div className="">
-                <i className="far fa-comment"></i> Comment
+                <div className="mb-2 text-capitalize font-weight-bold">
+                  {post.username}
+                </div>
+                <div className="postDateCreated">{post.dateCreated}</div>
               </div>
-            </Link>
+            </div>
+            <div className="post-description p-3 border-bottom">
+              {post.description}
+            </div>
+            <div className="post-photo container">
+              <img src={`data:image/jpeg;base64,${post.image}`} />
+            </div>
+            <div className="container post-footer d-flex justify-content-between px-4 py-2">
+              <div className="text-secondary">
+                0 <i className="fas fa-heart"></i>
+              </div>
+              <div className="text-secondary">2 Comments</div>
+            </div>
+            <div className="container border-top d-flex justify-content-between px-5 py-3">
+              <div className="likeCommentBtn">
+                <i className="far fa-thumbs-up"></i> Like
+              </div>
+              <Link to="/comments" className="likeCommentBtn">
+                <div className="">
+                  <i className="far fa-comment"></i> Comment
+                </div>
+              </Link>
+            </div>
           </div>
-        </div>
         ))}
-
 
         <div className="container">
           <Link to="/createposts" className="btn btn-success">
