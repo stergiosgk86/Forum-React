d0ed062b0cb67f9793ef94db31662d668b907c93
stergiosgk <52173282+stergiosgk86@users.noreply.github.com>
2021-09-05 21:44:31 +0300

    Notifications & interceptor for errors added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import ErrorHandler from "./ErrorHandler/ErrorHandler";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <BrowserRouter>
+    <ErrorHandler />
+    <ToastContainer />
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
